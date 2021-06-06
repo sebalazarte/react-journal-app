ca04b145ab2129d1112b156e15325d563ae60ee5
Andres Sebastian Lazarte <sebalazarte@gmail.com>
2021-06-05 21:17:46 -0300

    congiguracion de redux devtools

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { AppRouter } from './routers/AppRouter';
 import './styles/styles.scss';
+import { Provider } from 'react-redux'
+import { store } from './store/store';
 
 export const JournalApp = () => {
     return (
-        <AppRouter/>
+        <Provider store={store}>
+            <AppRouter  />
+        </Provider>
     )
 }
