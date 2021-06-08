1973feada4750128ea809fde969a773ec6ef40a2
Andres Sebastian Lazarte <sebalazarte@gmail.com>
2021-06-07 21:42:34 -0300

    note entry load

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { useSelector } from 'react-redux'
+import { useForm } from '../../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar'
 
 export const NoteScreen = () => {
+
+    const { active: note } = useSelector(state => state.notes);
+    const [values, handleInputChange, reset] = useForm(note);
+    const { body, title, url } = values;
+
+    const activeId = useRef(note.id);
+
+    useEffect(() => {
+        if(note.id !== activeId.current){
+            reset(note);
+            activeId.current = note.id
+        }
+    }, [note, reset])
+
     return (
         <div className="notes__main-content">
             <NotesAppBar />
@@ -11,18 +27,26 @@ export const NoteScreen = () => {
                     placeholder="some awsone title"
                     className="notes__title-input"
                     autoComplete="off"
+                    value={title}
+                    onChange={handleInputChange}
                 />
                 <textarea
                     placeholder="What happend today?"
-                    className="notes__textarea">
+                    className="notes__textarea"
+                    value={body}
+                    onChange={handleInputChange}>
                 </textarea>
 
-                <div className="notes__image">
-                    <img
-                        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
-                        alt="arbol"
-                    />
-                </div>
+                {
+                    url &&
+                    <div className="notes__image">
+                        <img
+                            src={url}
+                            alt="arbol"
+                        />
+                    </div>
+                }
+
             </div>
         </div>
     )
